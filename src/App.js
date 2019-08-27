81be676a6d50b7f9c7e86e3295fd4c3d208d2af2
vitor <vitorpaulasantos@gmail.com>
2019-08-27 09:13:05 -0300

    adicionando um style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import Form from './components/Form/index';
 import Header from './components/Header/index';
+import Styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
+    <Wrapper className="App">
       <Header />
       <Form />
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = Styled.div`
+  background: #00b894;
+  display: grid;
+  grid-template-rows: 20vh 70vh
+  grid-template-areas: "h"
+                       "f";
+                       
+  Header{
+    grid-area: h;
+    background: #00b894;
+  }
+  Form{
+    grid-area: f;
+    background: #dfe6e9;
+  }
+`;
 
 export default App;
